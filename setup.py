0e830d47531219916117d2acdebab7916c48bf78
Verdan Mahmood <verdan.mahmood@gmail.com>
2019-05-10 12:41:31 +0200

    Updates setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(requirements_path) as requirements_file:
+    requirements = requirements_file.readlines()
+
+__version__ = '0.0.1'
+
+setup(
+    name='flaskoidc',
+    version=__version__,
+    description='Flask wrapper with pre-configured OIDC support',
+    url='https://github.com/verdan/flaskoidc.git',
+    maintainer='Verdan Mahmood',
+    maintainer_email='verdan.mahmood@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    dependency_links=[],
+    install_requires=requirements,
+    python_requires=">=3.6",
+
+)
