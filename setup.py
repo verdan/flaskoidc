dff3c5f913881f7364000e9dfa0cdf52adf44d8c
Verdan Mahmood <verdan.mahmood@gmail.com>
2021-04-08 10:58:08 +0200

    Bump the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'r
 with open(requirements_path) as requirements_file:
     requirements = requirements_file.readlines()
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 setup(
     name='flaskoidc',
