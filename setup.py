070802b15df985502d4c3b178e759c50757b1576
verdan <verdan.mahmood@gmail.com>
2021-06-15 13:53:11 +0200

    updates the version
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = '0.3.0'
+__version__ = '1.0.0'
 
 setup(
     name='flaskoidc',
