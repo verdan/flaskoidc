9b1c668eaa12628e437ea176b587128171ae5e6b
verdan <verdan.mahmood@gmail.com>
2021-06-15 13:42:22 +0200

    fixes
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- flaskoidc/config.py
+++ flaskoidc/config.py
@@ -33,13 +33,10 @@ class BaseConfig(object):
     OIDC_PROVIDER = os.environ.get('FLASK_OIDC_PROVIDER_NAME', 'google')
     OIDC_SCOPES = os.environ.get('FLASK_OIDC_SCOPES', 'openid email profile')
     USER_ID_FIELD = os.environ.get('FLASK_OIDC_USER_ID_FIELD', 'email')
-    CLIENT_ID = os.environ.get('FLASK_OIDC_CLIENT_ID',
-                                           '434345347485-0ikvlscgvfp9jnfn9cj3f7pk3cq6jebq.apps.googleusercontent.com')
-    CLIENT_SECRET = os.environ.get('FLASK_OIDC_CLIENT_SECRET',
-                                               'rv5X0ZFpaVnTCIb-ojiV23f-')
+    CLIENT_ID = os.environ.get('FLASK_OIDC_CLIENT_ID', '')
+    CLIENT_SECRET = os.environ.get('FLASK_OIDC_CLIENT_SECRET', '')
     REDIRECT_URI = os.environ.get('FLASK_OIDC_REDIRECT_URI', '/auth')
-    CONFIG_URL = os.environ.get('FLASK_OIDC_CONFIG_URL',
-                                            'https://accounts.google.com/.well-known/openid-configuration')
+    CONFIG_URL = os.environ.get('FLASK_OIDC_CONFIG_URL', '')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', False)
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", 'sqlite:///sessions.db')
